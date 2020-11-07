0f04cd214dac705dd75c6ec73a74e1ddaa982ec4
William Roger <williamroger.frontend@gmail.com>
2020-11-07 14:55:19 -0300

    Adicionado Estrutura inicial

---
+++ src/components/Layout/styles.ts
@@ -0,0 +1,30 @@
+import { Column } from './../MiddleColumn/FeedPost/styles';
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  width: 100%;
+  height: 100%;
+
+  @media (min-width: 1180px) {
+    > main {
+      margin: 0 30px;
+      display: flex;
+      justify-content: center;
+    }
+  }
+  
+  .left-column,
+  .right-Column {
+    display: none;
+  }
+
+  @media(min-width: 1180px) {
+    .left-column, .right-Column {
+      display: unset;
+    }
+
+    .middle-column {
+      margin: 0 25px 16px;
+    }
+  }
+`;
---
+++ src/components/MobileHeader/index.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+// import { Container } from './styles';
+
+const MobileHeader: React.FC = () => {
+  return <div />;
+}
+
+export default MobileHeader;
