fdacc6eeba653b41899611a1f94f06fe60e9258f
William Roger <williamroger.frontend@gmail.com>
2020-11-07 15:40:00 -0300

    Finalizado componente Mobile Header

--- src/components/MobileHeader/index.tsx
+++ src/components/MobileHeader/index.tsx
@@ -1,9 +1,20 @@
 import React from 'react';
 
-// import { Container } from './styles';
+import { 
+  Container, 
+  ProfileCircle, 
+  SearchInput, 
+  MessageIcon 
+} from './styles';
 
 const MobileHeader: React.FC = () => {
-  return <div />;
+  return (
+    <Container>
+      <ProfileCircle src="https://github.com/williamroger.png" />
+      <SearchInput placeholder="Pesquisar" />
+      <MessageIcon />
+    </Container>
+  );
 }
 
 export default MobileHeader;
